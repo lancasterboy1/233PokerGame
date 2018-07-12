286351e8b14bdbb45e0085be989c002027d8388b
Marc Weber <marc.weber1@cs.ucalgary.ca>
2018-07-11 18:16:32 -0600

    Added card class and made globals a class

---
+++ PokerServer/Card.java
@@ -0,0 +1,15 @@
+public class Card extends Globals{
+	
+	private int faceVal;
+	private int suitVal;
+	
+	public Card(int face,int suit){
+		//Check if face is an index of FACE_VALUES and suit is an index of SUIT_VALUES
+		this.faceVal=face;
+		this.suitVal=suit;
+	}
+	
+	public String getName(){
+	return FACE_VALUES[face]+" of "+SUITS[suit];
+	}
+}
--- PokerServer/Globals.java
+++ PokerServer/Globals.java
@@ -4,4 +4,6 @@ public class Globals{
 	final static int EXPECTED_USERS=10;
 	final static int USER_VECTOR_CAPACITY_INCREMENT=5;
 	final static int MAX_INPUT_CHARACTERS=1024;
+	final static String[] FACE_VALUES=["2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"];
+	final static String[] SUITS=["diamonds","clubs","spades","hearts"];
 }
